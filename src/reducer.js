054ba40b9575a97a78e7490c360f56dc23e7df75
Camille <83260908+Camille846@users.noreply.github.com>
2022-08-23 22:39:35 -0300

    Update reducer const

--- src/reducer.js
+++ src/reducer.js
@@ -5,6 +5,10 @@ export const initialState = {
 }
 
 // Selector
+export const getBasketTotal = (basket) =>{
+    return(basket?.reduce((amount, item) => item.price + amount, 0))
+}
+
 const reducer = (state, action) => {
     switch(action.type) {
         case "ADD_TO_BASKET":
@@ -12,6 +16,26 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             }
+        case "REMOVE_FROM_BASKET":
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            )
+
+            let newBasket = [...state.basket]
+
+            if(index>=0){
+                newBasket.splice(index, 1)
+            } else {
+                console.warn(
+                    "cant remove product(id: ${action.id}) as it's not in the basket"
+                )
+            }
+            return{
+                ...state,
+                basket: newBasket
+            }
+        default: 
+            return state;
     }
 }
 
