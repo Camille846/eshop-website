974215be268c4a9a3659bb9ed1c785ce3fba817d
Camille <83260908+Camille846@users.noreply.github.com>
2022-08-23 22:35:36 -0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqFC3Iqjo93BBsMA3FnN34cAFLEElRXa4",
+    authDomain: "eshop-e8127.firebaseapp.com",
+    projectId: "eshop-e8127",
+    storageBucket: "eshop-e8127.appspot.com",
+    messagingSenderId: "612051849002",
+    appId: "1:612051849002:web:849b042689ab75f4375853",
+    measurementId: "G-VLJHZB32K9"
+  
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { auth, database, app };
