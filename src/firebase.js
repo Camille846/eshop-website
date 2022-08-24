5a46c7a65b6f122217a47a6c8ed1962620d98035
Camille <83260908+Camille846@users.noreply.github.com>
2022-08-23 23:01:36 -0300

    Update project dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
     messagingSenderId: "612051849002",
     appId: "1:612051849002:web:849b042689ab75f4375853",
     measurementId: "G-VLJHZB32K9"
-  
 };
 
 const app = initializeApp(firebaseConfig);
